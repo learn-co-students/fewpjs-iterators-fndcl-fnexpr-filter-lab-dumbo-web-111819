6e7978665b9a67669e670ebf8d31ce39317753f9
“Saima <rahman.saima92@gmail.com>
2020-01-23 10:28:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter((ele) => {
+      return ele.toLowerCase() === string.toLowerCase()
+    })
+}
+
+const fuzzyMatch = (drivers, string) => {
+  return drivers.filter((ele) => {
+    return ele[0] === string[0]
+  })
+}
+
+const matchName = (drivers, string) => {
+  return drivers.filter((ele) => {
+      return ele.name === string
+  })
+}
